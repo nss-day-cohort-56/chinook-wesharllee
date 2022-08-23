f830a40b2fe3b3db625e038c0ac37ce768317205
Wesley Harllee <wesharllee@gmail.com>
2022-08-23 15:00:56 -0500

    sales_agent_total_sales

---
+++ sales_agent_total_sales.sql
@@ -0,0 +1,9 @@
+SELECT 
+    e.firstname || " " || e.lastname as fullname,
+    SUM(i.total) totalsales
+FROM employee e 
+JOIN customer c 
+    on e.employeeid = c.supportrepid
+JOIN invoice i 
+    on i.customerid = c.customerid
+GROUP BY e.EmployeeId
